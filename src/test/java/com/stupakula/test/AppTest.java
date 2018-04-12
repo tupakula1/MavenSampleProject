7f949c6b0bd9260739795a2aea4a64ee076eb2f6
Srinivasulu T <srini.myinfo@gmail.com>
2018-04-12 12:35:06 +0530

    AppTestt
    AppTest file

---
+++ src/test/java/com/stupakula/test/AppTest.java
@@ -0,0 +1,20 @@
+package com.stupakula.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       System.out.println("Hell Test");
+    	Assert.assertTrue( true );
+    }
+}
